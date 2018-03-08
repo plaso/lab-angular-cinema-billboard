c76b35ca7b6bec025376a7e5f7742bd56ea84703
Pablo Laso <plasocortabitarte@gmail.com>
2018-03-08 21:50:49 +0100

    model and data

---
+++ starter-code/src/app/shared/models/movie.model.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  id: number;
+  title: string;
+  poster: string;
+  synopsis: string;
+  genres: Array<string>;
+  year: number;
+  director: string;
+  actors: Array<string>;
+}
