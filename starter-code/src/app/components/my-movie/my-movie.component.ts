e039e28809bb875a50613ed6d8c5089ca352d51f
Pablo Laso <plasocortabitarte@gmail.com>
2018-03-09 11:35:31 +0100

    iteration 4

--- starter-code/src/app/components/my-movie/my-movie.component.ts
+++ starter-code/src/app/components/my-movie/my-movie.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../../shared/services/movies.service';
+import { Movie } from '../../shared/models/movie.model';
 
 @Component({
   selector: 'app-my-movie',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-movie.component.css']
 })
 export class MyMovieComponent implements OnInit {
+  movie: Movie;
 
-  constructor() { }
+  constructor(
+    private routes: ActivatedRoute,
+    private moviesService: MoviesService
+  ) { }
 
   ngOnInit() {
+    this.routes.params.subscribe(params => {
+      this.movie = this.moviesService.getMovie(Number(params['id']));
+    });
   }
 
 }
